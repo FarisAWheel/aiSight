09cb12ca79094681b029f0d3c18e8e8168429743
Jeff <lijefferson027@gmail.com>
2025-02-17 19:55:42 -0500

    Made gradient background into a component so it can be reused

--- aiSightApp/pages/welcome_page.tsx
+++ aiSightApp/pages/welcome_page.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { AntDesign } from '@expo/vector-icons';
+import  GradientBackground  from '../components/GradientBackground'
 
+//welcome page
 const WelcomePage = () => {
     return (
-        <LinearGradient
-            colors={['#9FD2F0', '#BADFC0', '#9BB5DE']}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            style={styles.container}
-        >
+        <GradientBackground>
             {/* <Image source = {require('../assets/aiSIghtLogo.png')} style={styles.logo}/> */}
             
                 <Text style={styles.helloText}>Hello,</Text>
@@ -20,18 +16,11 @@ const WelcomePage = () => {
             <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonText}>Get started</Text>
             </TouchableOpacity>
-        </LinearGradient>
+        </GradientBackground>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        // paddingLeft: 50,
-    },
     helloText: {
         fontSize: 48,
         fontStyle: 'italic',
