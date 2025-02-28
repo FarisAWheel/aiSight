d5c32bd48228cad46c03be5ce9b6e17e870f4fa9
farisawheel <farishabdelrahem@gmail.com>
2025-02-28 15:30:50 -0500

    lol, this doesn't even work yet, but arudino ide setup stuff idk

---
+++ aiSightCam/aiSightCam.ino
@@ -0,0 +1,18 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ESPAsyncWebServer.h>
+#include "esp_camera.h"
+
+// Network Credentials
+const char* ssid = "ESP32-CAM";
+const char* password = "aVeryStrongPassword";
+
+// Create an access point
+
+void setup() {
+    
+}
+
+void loop() {
+    delay(100);`
+}
