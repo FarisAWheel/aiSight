3a72a3c3c47a88e21c4efbe086aff1a156f167b8
farisawheel <farishabdelrahem@gmail.com>
2025-03-09 14:00:56 -0400

    basic bluetooth functionality

---
+++ aiSightCam/camHandler.h
@@ -0,0 +1,10 @@
+#ifndef CAMHANDLER_H
+#define CAMHANDLER_H
+
+#include <WiFi.h>
+#include <WebServer.h>
+
+void initBLE(String password);
+
+
+#endif
---
+++ aiSightCam/camWebserver.cpp
@@ -0,0 +1,33 @@
+#include "camHandler.h"
+
+// temporary credentials
+const char* ssid = "aiSight";
+const char* password = "TheGreatestPasswordEver";
+
+// ip address information
+/* Put IP Address details */
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+WebServer server(80);
+
+void initWebserver() {
+  Serial.begin(115200);
+  pinMode(LED1pin, OUTPUT);
+  pinMode(LED2pin, OUTPUT);
+
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  
+  server.on("/", handle_OnConnect);
+  server.on("/led1on", handle_led1on);
+  server.on("/led1off", handle_led1off);
+  server.on("/led2on", handle_led2on);
+  server.on("/led2off", handle_led2off);
+  server.onNotFound(handle_NotFound);
+  
+  server.begin();
+  Serial.println("HTTP server started");
+}
