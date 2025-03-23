45c3cd34881367bf9cba4a48d21cc368148b26e7
Katherina Dayaon <katherinadayaon@gmail.com>
2025-03-23 17:42:46 -0400

    Added both delete and edit functionalities to page

--- aiSightApp/pages/peopleHistory_page.tsx
+++ aiSightApp/pages/peopleHistory_page.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import GradientBackground from '../components/GradientBackground'
-import { Text, View, TouchableOpacity, StyleSheet, Button, FlatList, Image} from 'react-native'
-import LeftBackArrowButton from '../components/LeftBackArrowButton'
-import RightArrowButton from '../components/RightArrowButton'
+import React, { useState } from 'react';
+import GradientBackground from '../components/GradientBackground';
+import { Text, View, StyleSheet, FlatList, Image, Alert, Modal, TextInput, Button } from 'react-native';
+import LeftBackArrowButton from '../components/LeftBackArrowButton';
 import { MenuProvider } from 'react-native-popup-menu';
 import {
     Menu,
@@ -10,90 +9,155 @@ import {
     MenuOption,
     MenuTrigger,
     renderers,
-  } from 'react-native-popup-menu'
-import Icon from 'react-native-vector-icons/FontAwesome'
+} from 'react-native-popup-menu';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const { Popover } = renderers;
 
-const HistoryPage = ({navigation}: {navigation:any}) =>{
-    return( 
+const HistoryPage = ({ navigation }: { navigation: any }) => {
+    // State to manage the list of people
+    const [people, setPeople] = useState([
+        { key: 'Faris' },
+        { key: 'Thomas' },
+        { key: 'Kat' },
+        { key: 'Jeff' },
+        { key: 'Isaiah' },
+        { key: 'Emilio' },
+    ]);
+
+    // State for editing
+    const [isEditing, setIsEditing] = useState(false);
+    const [currentPerson, setCurrentPerson] = useState< string | null>(null);
+    const [newName, setNewName] = useState('');
+
+    // Function to delete a person from the list
+    const deletePerson = (personKey: string) => {
+        setPeople((prevPeople) => prevPeople.filter((person) => person.key !== personKey));
+    };
+
+    // Function to START editing a person's name
+    const startEditing = (personKey: string) => {
+        setCurrentPerson(personKey);
+        const person = people.find((p) => p.key === personKey);
+        setNewName(person?.key || '');
+        setIsEditing(true);
+    };
+
+    // Function to save the edited name
+    const saveEdit = () => {
+        setPeople((prevPeople) =>
+            prevPeople.map((person) => 
+                person.key === currentPerson ? { ...person, key: newName } : person
+            )
+        );
+        setIsEditing(false);
+        setCurrentPerson(null);
+        setNewName('');
+    }
+
+    return (
         <MenuProvider>
             <GradientBackground>
-                <LeftBackArrowButton onPress={() => navigation.navigate('Home')}/>
+                <LeftBackArrowButton onPress={() => navigation.navigate('Home')} />
                 <View style={styles.boxShadow}>
-                    <View style={{flexDirection: 'column', alignItems: 'center', justifyContent: 'center', left: 10}}>
+                    <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center', left: 10 }}>
                         <Text style={styles.text}>People History</Text>
-                        <View style = {{flexDirection: 'row', width: '100%', height: '90%', top: 20, left: 30}}>
+                        <View style={{ flexDirection: 'row', width: '100%', height: '90%', top: 20, left: 30 }}>
                             <FlatList
-                            // hardcoded data for now
-                                data={[
-                                {key: 'Faris'},
-                                {key: 'Thomas'},
-                                {key: 'Kat'},
-                                {key: 'Jeff'},
-                                {key: 'Isaiah'},
-                                {key: 'Emilio'},
-                                {key: 'Faris'},
-                                {key: 'Thomas'},
-                                {key: 'Kat'},
-                                {key: 'Jeff'},
-                                {key: 'Isaiah'},
-                                {key: 'Emilio'},
-                                {key: 'Faris'},
-                                {key: 'Thomas'},
-                                {key: 'Kat'},
-                                {key: 'Jeff'},
-                                {key: 'Isaiah'},
-                                {key: 'Emilio'},
-                                ]}
+                                data={people}
                                 renderItem={({ item }) => (
                                     <View style={styles.listItem}>
                                         <View style={styles.bulletBox} />
                                         <Text style={styles.item}>{item.key}</Text>
                                         {/* The three buttons on the side for menu */}
-                                        <View style={{flexDirection: 'row', justifyContent: 'space-between', position: 'absolute', right: 20, width: '22%'}}>
-                                            <Menu 
+                                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', position: 'absolute', right: 20, width: '22%' }}>
+                                            <Menu
                                                 renderer={Popover}
                                                 rendererProps={{
-                                                    placement: 'bottom', // Position the menu below the trigger
-                                                    anchorStyle: { backgroundColor: 'white' }, // Customize the anchor
+                                                    placement: 'bottom',
+                                                    anchorStyle: { backgroundColor: 'white' },
+                                                    popoverStyle: {
+                                                        shadowColor: '#000',
+                                                        shadowOffset: { width: 0, height: 0 },
+                                                        shadowOpacity: 0.9,
+                                                        shadowRadius: 10,
+                                                        elevation: 10,
+                                                        backgroundColor: 'red',
+                                                        borderRadius: 8,
+                                                    },
                                                 }}
-                                                onSelect={value => alert(`Are you sure you want to delete ${value}?`)}
-                                                >
+                                                onSelect={(value) => {
+                                                    if (value.action === 'delete') {
+                                                        Alert.alert(
+                                                            "Caution",
+                                                            `Are you sure you want to delete ${value.person}?`,
+                                                            [
+                                                                { text: "Cancel", style: "cancel" },
+                                                                { text: "Yes", onPress: () => {
+                                                                    console.log(`Deleted ${value.person}`);
+                                                                    deletePerson(value.person);
+                                                                }},
+                                                            ]
+                                                        );
+                                                    }
+                                                    else if(value.action === 'edit') {
+                                                        startEditing(value.person);
+                                                    }
+                                                }}
+                                            >
                                                 <MenuTrigger>
                                                     <Image
-                                                            source={require('../assets/threeDots.png')}
-                                                            style={{width: 20, height: 20}}
-                                                        />
+                                                        source={require('../assets/threeDots.png')}
+                                                        style={{ width: 20, height: 20 }}
+                                                    />
                                                 </MenuTrigger>
                                                 <MenuOptions>
-                                                    <EditOption value={item.key} text='Edit name' />
-                                                    <DeleteOption value={item.key} text='Delete'>
-                                                    </DeleteOption>
+                                                    <EditOption value={item.key} text="Edit name" />
+                                                    <DeleteOption value={item.key} text="Delete" />
                                                 </MenuOptions>
                                             </Menu>
                                         </View>
                                     </View>
-                                )} 
-                                />
-                            </View>
+                                )}
+                            />
+                        </View>
                     </View>
                 </View>
             </GradientBackground>
+
+            {/* Modal for editing the name */}
+            <Modal visible={isEditing} animationType="slide" transparent={true}>
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalTitle}>Edit Name</Text>
+                        <TextInput
+                            style={styles.input}
+                            value={newName}
+                            onChangeText={setNewName}
+                            placeholder="Enter new name"
+                        />
+                        <View style={styles.modalButtons}>
+                            <Button title="Cancel" onPress={() => setIsEditing(false)} color={'#555555'}/>
+                            <Button title="Save" onPress={saveEdit} color={'#9BB5DE'} />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </MenuProvider>
-    )
-}
+    );
+};
 
 const DeleteOption: React.FC<{ value: string; text: string }> = (props) => (
-    <MenuOption value={props.value}>
+    <MenuOption value={{ action: 'delete', person: props.value }}>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
             <Icon name="trash" size={16} color="#F17070" style={{ marginRight: 8 }} />
             <Text>{props.text}</Text>
         </View>
     </MenuOption>
 );
+
 const EditOption: React.FC<{ value: string; text: string }> = (props) => (
-    <MenuOption value={props.value}>
+    <MenuOption value={{ action: 'edit', person: props.value }}>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
             <Icon name="pencil" size={16} color="#000" style={{ marginRight: 8 }} />
             <Text>{props.text}</Text>
@@ -119,7 +183,7 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 25,
-        fontFamily: 'SF Pro Display', 
+        fontFamily: 'SF Pro Display',
         fontWeight: '700',
         color: '#555555',
     },
@@ -138,7 +202,39 @@ const styles = StyleSheet.create({
         padding: 10,
         fontSize: 18,
         height: 44,
-      },
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        width: '80%',
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    modalTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        color: '#555555',
+    },
+    input: {
+        width: '100%',
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 20,
+    },
+    modalButtons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+    },
 });
 
-export default HistoryPage
+export default HistoryPage;
