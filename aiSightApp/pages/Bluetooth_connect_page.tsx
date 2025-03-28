173e7c8645b04a4dde961a51fb4b0c298a3d0228
farisawheel <farishabdelrahem@gmail.com>
2025-03-28 15:56:26 -0400

    set up temporary route after conncetion and updated dependencies

--- aiSightApp/pages/Bluetooth_connect_page.tsx
+++ aiSightApp/pages/Bluetooth_connect_page.tsx
@@ -72,7 +72,7 @@ const BluetoothPage = ({ navigation }: { navigation: any }) => {
           </View>
           <Pressable
             style={styles.nextButton}
-            onPress={() => navigation.navigate("Home")}
+            onPress={() => navigation.navigate("Welcome")} // when home is implemented, direct there
           >
             <Ionicons name="arrow-forward" size={30} color="black" />
           </Pressable>
