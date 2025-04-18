7c181e6d4564ede4ada8eb5ffbaa060cabf0ea15
farisawheel <farishabdelrahem@gmail.com>
2025-04-18 05:56:48 -0400

    working on it!

--- aiSightApp/metro.config.js
+++ aiSightApp/metro.config.js
@@ -1,14 +1,16 @@
+/** @type {import('expo/metro-config').MetroConfig} */
+
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const {
   wrapWithReanimatedMetroConfig,
 } = require("react-native-reanimated/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-module.exports = wrapWithReanimatedMetroConfig(config);
-const { getDefaultConfig } = require("@expo/metro-config");
+// Get the default config
 const defaultConfig = getDefaultConfig(__dirname);
+
+// Add ONNX to the list of asset extensions
 defaultConfig.resolver.assetExts.push("onnx");
-module.exports = defaultConfig;
+
+// Apply Reanimated wrapper and export only once
+module.exports = wrapWithReanimatedMetroConfig(defaultConfig);
